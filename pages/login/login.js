90eb63158f2443bf42681d97ee83a64d50b67c0d
Dima <onridge7311@gmail.com>
2019-08-18 23:34:03 +0300

    update

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,9 +3,36 @@ const feedHeader = document.getElementById('feedHeader');
 const mainHeader = document.getElementById('header');
 const router = '#/feed';
 
+
+function doRequest(data) {
+    fetch('https://intern-staging.herokuapp.com/api/identification/sign_in',{
+        method:'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then(
+        resp => resp.json()
+    ).then(
+        json =>{
+//            if (json.token){
+                localStorage.setItem('token', json.token);
+                mainHeader.style.display = 'none';
+                feedHeader.style.display = 'block';
+                window.location.href = router;
+
+//            }
+        }
+
+    ).catch(
+        resp => alert(resp)
+    );
+}
+
 loginBtn.addEventListener('click', function () {
-    mainHeader.style.display = 'none';
-    feedHeader.style.display = 'block';
-    window.location.href = router;
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    let newUser = {email:email, password: password};
+    doRequest(newUser);
 });
 
